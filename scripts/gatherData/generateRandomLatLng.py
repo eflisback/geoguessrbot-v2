c83560188cfb2886a0032481c6da129e7a1966d7
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2023-04-22 16:43:38 +0200

    Comments & cleanup: generateRandomLatLng.py

--- scripts/gatherData/generateRandomLatLng.py
+++ scripts/gatherData/generateRandomLatLng.py
@@ -1,9 +1,11 @@
-import random
 from shapely import MultiPoint
-from shapely.geometry import Point
 import numpy as np
 
 
+# This script generates random points within a specified country's boundary
+# using the Shapely library to handle geometric operations.
+
+# Class for defining console text colors and styles.
 class BColors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -16,19 +18,31 @@ class BColors:
     UNDERLINE = '\033[4m'
 
 
+# Function to generate random points within the boundary of a given country
+# Inputs:
+#   country: Shapely geometry object representing the country's boundary
+#   n: number of random points to generate
 def random_points_within_country(country, n):
+    # Get the bounding box of the country
     min_x, min_y, max_x, max_y = country.bounds
 
     result_points = []
     while len(result_points) < n:
         num_points_to_generate = n - len(result_points)
         print(f"   Generating {num_points_to_generate} random points in rectangle...")
+
+        # Generate random x and y coordinates within the bounding box
         random_x = np.random.uniform(min_x, max_x, num_points_to_generate)
         random_y = np.random.uniform(min_y, max_y, num_points_to_generate)
+
+        # Create MultiPoint geometry from the random x and y coordinates
         random_points = MultiPoint(np.column_stack((random_x, random_y)))
 
+        # Filter points that are contained within the country's boundary
         contained_points = [point for point in random_points.geoms if country.contains(point)]
 
+        # Add contained points to the result_points list
         result_points.extend(contained_points)
 
+    # Convert the result_points list to a numpy array and return it
     return np.array([(point.y, point.x) for point in result_points])
